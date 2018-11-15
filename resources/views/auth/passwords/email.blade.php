ebfcc2e4bff8fc49ac6f1fa5455bb9603020e4b1
Falco Rodenburg <falcorodenburg@gmail.com>
2018-11-15 12:25:36 +0100

    password reset updated

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -2,14 +2,11 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
+    <div class="login-form">
+        <h2>{{ __('Reset Password') }}</h2>
+        <p id="email-text">Fill in your email-address so we can send you a link to reset your password</p>
+            @if (session('status'))
+            <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
@@ -18,30 +15,35 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
-                                @if ($errors->has('email'))
+                                <!--@if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
+                                @endif-->
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
+                        <div class="login">
+                            <button type="submit" class="login-btn">
+                                {{ __('Send') }}
+                            </button>
                         </div>
                     </form>
+                    @if ($errors->any())
+    <div class="notification">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
                 </div>
             </div>
         </div>
-    </div>
-</div>
 @endsection

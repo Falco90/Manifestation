ba9f47331a60d76f168cb5768eb18bd05b6a1deb
Falco Rodenburg <falcorodenburg@gmail.com>
2018-11-16 11:53:59 +0100

    updated lorem ipsum texts with other placeholder content

--- resources/views/instructions.blade.php
+++ resources/views/instructions.blade.php
@@ -5,6 +5,8 @@
 @section('content')
 <h2>Instructions</h2>
     <h3>"You can't just visualize and go eat a sandwich" - Jim Carrey</h3>
-    <p>Here I will write some instructions. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam erat libero, facilisis at metus aliquam, sagittis consequat turpis. Proin vel nisl lacinia, semper tortor iaculis, eleifend ipsum. Vestibulum laoreet vel leo sit amet ultricies. Nunc quam turpis, aliquet sed bibendum vitae, dignissim sit amet massa.
+    <p>Here I will write some instruction on how to manifest. Will get updated soon!<br><br> 
+        The Law of Attraction involves more than just spending a few minutes visualizing your goals, it's the entire creation process that manifests your desires. You have to believe it and take action towards your desire. <br><br> 
+        You create your entire reality. Every event that takes place, every experience you have – they are all your creation. You are the creator of everything through your intentions and desires. Whether it's the most amazing experience of your life or a crappy day at work, you are the creator. In every moment, ask yourself, “What do I want to create?”
     </p>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
             {{ session('status') }}
         </div>
     @endif
-    <p>You are now logged in to the Floating Oasis! Now you can start manifesting. <br><br>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam erat libero, facilisis at metus aliquam, sagittis consequat turpis. Proin vel nisl lacinia, semper tortor iaculis, eleifend ipsum. Vestibulum laoreet vel leo sit amet ultricies. Nunc quam turpis, aliquet sed bibendum vitae, dignissim sit amet massa.</p>
+    <p>You are now logged in to the Floating Oasis!<br><br>
+        More content will be coming soon!
+    </p>
 @endsection

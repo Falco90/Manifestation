cbb08526554cfa2d092941b77a0f0d797503af97
Falco Rodenburg <falcorodenburg@gmail.com>
2018-11-16 11:42:04 +0100

    added username to welcome screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,12 @@
 @extends('layout')
 
 @section('content')
-<h1>Welcome</h1>
+<h2>Welcome {{ Auth::user()->name }}!</h2>
     @if (session('status'))
         <div class="alert alert-success" role="alert">
             {{ session('status') }}
         </div>
     @endif
-    <p>You are now logged in to the Floating Oasis! Now you can start manifesting. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam erat libero, facilisis at metus aliquam, sagittis consequat turpis. Proin vel nisl lacinia, semper tortor iaculis, eleifend ipsum. Vestibulum laoreet vel leo sit amet ultricies. Nunc quam turpis, aliquet sed bibendum vitae, dignissim sit amet massa.</p>
+    <p>You are now logged in to the Floating Oasis! Now you can start manifesting. <br><br>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam erat libero, facilisis at metus aliquam, sagittis consequat turpis. Proin vel nisl lacinia, semper tortor iaculis, eleifend ipsum. Vestibulum laoreet vel leo sit amet ultricies. Nunc quam turpis, aliquet sed bibendum vitae, dignissim sit amet massa.</p>
 @endsection
